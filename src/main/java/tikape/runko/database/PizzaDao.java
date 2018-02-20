6dcd05dccc81e882404ba253aa9328eff02ef3ce
胡 滢滢 <huyingying@1BQQGRC.kumpula.dhcp.hupnet.helsinki.fi>
2018-02-20 11:43:55 +0200

    jj

---
+++ src/main/java/tikape/runko/database/PizzaDao.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.runko.database;
+
+import java.sql.SQLException;
+import java.util.List;
+import tikape.runko.domain.Pizza;
+
+/**
+ *
+ * @author huyingying
+ */
+public class PizzaDao implements Dao<Pizza, Integer> {
+    private Database db;
+    
+    public PizzaDao(Database db) {
+        this.db = db;
+    }
+
+    @Override
+    public Pizza findOne(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Pizza> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
