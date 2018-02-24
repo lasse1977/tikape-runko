ec0195e4d6f88dc634a178fe3f1912e26581233d
胡 滢滢 <huyingying@kotekiMBP.home>
2018-02-24 17:55:21 +0200

    luokka pizza

--- src/main/java/tikape/runko/domain/Pizza.java
+++ src/main/java/tikape/runko/domain/Pizza.java
@@ -13,7 +13,31 @@ import java.util.List;
  */
 public class Pizza {
     
-    String nimi;
-    List<RaakaAine> ainekset;
+    private Integer id;
+    private String nimi;
+    private List<RaakaAine> ainekset;
+    
+
+    public Pizza(Integer id, String string) {
+        this.id = id;
+        this.nimi = nimi;
+    }
+    
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+    
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+    
     
 }
