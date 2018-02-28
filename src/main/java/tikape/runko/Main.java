28c370666e822f155243e0b4b7d526995050b194
Lasse Niemistö <nielasse@dhcp-asv-121.helsinki.fi>
2018-02-28 11:13:08 +0200

    testausta

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -8,10 +8,15 @@ import tikape.runko.database.Database;
 import tikape.runko.database.PizzaDao;
 import tikape.runko.database.RaakaAineDao;
 import tikape.runko.domain.Pizza;
+import tikape.runko.domain.RaakaAine;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        if (System.getenv("PORT") != null) {
+        port(Integer.valueOf(System.getenv("PORT")));
+        }
+        
         Database database = new Database("jdbc:sqlite:pizzat.db");
         database.init();
         
@@ -37,8 +42,16 @@ public class Main {
             String nimi = req.queryParams("nimi");
             System.out.println("Vastaanotettiin " + nimi);
             pizzaDao.save(new Pizza(1, nimi));
-
-            return nimi + " lisätty";
+            res.redirect("/pizzat");
+            return "";
+        });
+        
+        post("/taytteet", (req, res) -> {
+            String nimi = req.queryParams("nimi");
+            System.out.println("Vastaanotettiin " + nimi);
+            raakaAineDao.save(new RaakaAine(1, nimi));
+            res.redirect("/taytteet");
+            return "";
         });
         
         get("/taytteet", (req, res) -> {
